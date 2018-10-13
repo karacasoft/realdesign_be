2e9c4e843ce81072177dcba42824e4f203547eaa
Özge Dinçsoy <ozgedincsoy@a-MacBook-Pro.local>
2018-10-13 18:33:12 +0300

    image posting is done

--- webservice/index.js
+++ webservice/index.js
@@ -11,8 +11,36 @@ app.use(cors({
     origin: "*"
 }))
 
-router.get( '/', async (ctx, next) => {
-    ctx.body = "hello"
+var base64Img = require('base64-img');
+
+/** 
+function postFirstParagraph(ctx) {
+    return new Promise((fulfill, reject) => {
+        console.log("girdi2")
+        var body = ctx.request.body;
+        base64Img.img('data:image/png;base64,...', 'dest', '1', function(err, filepath) {});
+        fulfill(body);
+    });
+  }
+*/
+
+router.get( '/abc' , async (ctx,next) => {
+    //var body = ctx.request.body;
+    console.log("body i yazıyor")
+    //base64Img.img(body.img, 'dest', '2', function(err, filepath) {});
+    ctx.body = "wow";
+    //const results = await postFirstParagraph(ctx);
+    //ctx.redirect("http://localhost:8000");
+});
+
+router.post( '/detect' , koaBody(), async (ctx,next) => {
+    var body = ctx.request.body;
+    console.log("body i yazıyor")
+    console.log(JSON.stringify(body));
+    //base64Img.img(body.img, 'dest', '2', function(err, filepath) {});
+    ctx.body = "wow";
+    //const results = await postFirstParagraph(ctx);
+    //ctx.redirect("http://localhost:8000");
 });
 
 app.use(router.routes())
